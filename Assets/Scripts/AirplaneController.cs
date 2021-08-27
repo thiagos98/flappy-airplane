30ccbfddf860de4d321a3c1d2d7a2bd94ca22f0e
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-08-26 20:07:53 -0400

    Initial commit

---
+++ Assets/Scripts/AirplaneController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneController : MonoBehaviour
+{
+    [SerializeField] private float impulse;
+    private Rigidbody2D _mRigidbody;
+    
+    private void Awake()
+    {
+        _mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var space = Input.GetButtonDown("Jump");
+        if (space)
+        {
+            Boost();
+        }
+    }
+
+    private void Boost()
+    {
+        _mRigidbody.AddForce(Vector2.up * impulse, ForceMode2D.Impulse);
+    }
+}
